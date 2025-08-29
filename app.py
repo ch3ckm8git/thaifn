ac7c727d9ba580cf1fa7599a1181745853cac28e
Check <check@Checks-MacBook-Pro.local>
2025-08-29 17:36:41 +0700

    ah

--- app.py
+++ app.py
@@ -1,3 +1,53 @@
+# from flask import Flask, request, jsonify
+# import joblib
+# import pandas as pd
+# import re, string
+# from nltk.corpus import stopwords
+# from nltk.stem import WordNetLemmatizer
+# from flask_cors import CORS
+# import nltk
+
+# nltk.data.path.append("./nltk_data")
+# model = joblib.load("./fake_news_model.pkl")
+
+# # Preprocessing setup
+# stop_words = set(stopwords.words("english"))
+# lemmatizer = WordNetLemmatizer()
+
+# def clean_single_text(text):
+#     text = text.lower()
+#     text = re.sub(f"[{re.escape(string.punctuation)}]", "", text)
+#     tokens = text.split()
+#     tokens = [t for t in tokens if t not in stop_words]
+#     tokens = [lemmatizer.lemmatize(t) for t in tokens]
+#     return " ".join(tokens)
+
+# # Flask app
+# app = Flask(__name__)
+# CORS(app) 
+# @app.route("/predict", methods=["POST"])
+# def predict():
+#     data = request.get_json()
+#     title = data.get("title", "")
+#     text = data.get("text", "")
+    
+#     clean_text = clean_single_text(text)
+    
+#     df = pd.DataFrame([{
+#         "title": title,
+#         "text_tokens_str": clean_text
+#     }])
+
+#     pred = model.predict(df)[0]
+#     proba = model.predict_proba(df)[0].tolist()
+
+#     return jsonify({
+#         "prediction": int(pred),
+#         "confidence": proba
+#     })
+
+# if __name__ == "__main__":
+#     app.run(debug=False)
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
@@ -25,6 +75,7 @@ def clean_single_text(text):
 # Flask app
 app = Flask(__name__)
 CORS(app) 
+
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
@@ -47,4 +98,4 @@ def predict():
     })
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=5000)
